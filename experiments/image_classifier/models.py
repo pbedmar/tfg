2026374caff517357581d9bb3d40215e73c56da3
Pedro Bedmar Lopez <correo.pedrobedmar@gmail.com>
2022-05-14 17:39:55 +0200

    grid search and train are now different scripts

--- experiments/image_classifier/models.py
+++ experiments/image_classifier/models.py
@@ -105,24 +105,24 @@ class AlexNet(nn.Module):
         return x
 
 
-class LeNet(nn.Module):
+class LeNet5(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 6, kernel_size= 5, padding=2)
         self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
 
-        self.fc1 = nn.Linear(200, 120)
+        self.fc1 = nn.Linear(61504, 120)
         self.fc2 = nn.Linear(120,84)
         self.fc3 = nn.Linear(84, 2)
 
     def forward(self,x):
-        x = F.sigmoid(self.conv1(x))
+        x = torch.sigmoid(self.conv1(x))
         x = F.max_pool2d(x, kernel_size=2, stride=(2, 2), dilation=(1, 1))
-        x = F.sigmoid(self.conv2(x))
+        x = torch.sigmoid(self.conv2(x))
         x = F.max_pool2d(x, kernel_size=2, stride=(2, 2), dilation=(1, 1))
 
         x = x.reshape(x.shape[0], -1)
-        x = F.sigmoid(self.fc1(x))
-        x = F.sigmoid(self.fc2(x))
+        x = torch.sigmoid(self.fc1(x))
+        x = torch.sigmoid(self.fc2(x))
         x = self.fc3(x)
         return x
---
+++ experiments/image_classifier/train.py
@@ -0,0 +1,43 @@
+from datasets import CompoundDataset
+from functions import train, test
+import models
+
+import torch
+from torch.utils.data import DataLoader
+from torchvision import transforms
+
+torch.manual_seed(0)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+train_dataset = CompoundDataset(
+                            "../../datasets/negev/articles_molecules/preprocess256/aug2",
+                            "../../datasets/negev/not_molecules/preprocess256",
+                            "train",
+                            transform=transforms.ToTensor()
+                          )
+
+test_dataset = CompoundDataset(
+                            "../../datasets/negev/articles_molecules/preprocess256/aug2",
+                            "../../datasets/negev/not_molecules/preprocess256",
+                            "test",
+                            transform=transforms.ToTensor()
+                          )
+
+train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=32)
+
+model = models.LeNet5()
+model.to(device)
+criterion = torch.nn.CrossEntropyLoss()
+nb_epochs = 100
+lr = 1e-4
+
+train(train_dataloader, model, criterion, nb_epochs, lr, device)
+torch.save(model.state_dict(), "saved_models/lenettry.pt")
+
+nb_errors = test(test_dataloader, model, device)
+
+print("Test dataset size:", len(test_dataset))
+print("Total errors predicting test dataset:", nb_errors)
+print("Error percentage:", nb_errors/len(test_dataset)*100)
